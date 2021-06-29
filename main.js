14b14c5a079fd83fdaba6d07451ebc2db4b68a20
Giorgi-NADIRADZ3 <82814663+Giorgi-NADIRADZ3@users.noreply.github.com>
2021-06-29 21:44:19 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function imgslider(anything){
+    document.querySelector('.cup').src = anything; 
+}
+function changeCircleColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.backround = color;
+}
